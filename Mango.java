f74fec99428c87b2859a518e1ca961491e89ec26
Pranav <130391920+PranavSystem@users.noreply.github.com>
2023-04-25 00:58:25 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,27 @@
+package Fruits;
+
+public class Mango extends Fruit{
+	
+	public Mango()
+	{
+		
+	}
+	public Mango(String color, String name, double weight, boolean fresh)
+	{
+		super(color,name,weight,fresh);
+		System.out.println("............");
+	}
+		
+
+	
+	public String toString()
+	{
+		
+		return super.toString();
+	}
+	
+	
+	
+	
+	
+}
